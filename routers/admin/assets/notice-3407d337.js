615835a4b68d5b44a7fc44a9bb962a429ecc1837
yuwnloy <yuwnloyblog@163.com>
2026-06-01 15:08:29 +0800

    opt admin

--- routers/admin/assets/notice-3407d337.js
+++
@@ -1,1 +0,0 @@
-import{s as t,o as e}from"./index-ca1cebfd.js";import{_ as o}from"./page-section-b47fddc5.js";const n={__name:"notice",setup(c){return(r,s)=>(e(),t(o,{"title-key":"tools.notice.title"}))}};export{n as default};
